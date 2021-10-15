0fdfb4e32653024195c2c11ee25a93b4ce5cfd60
team-CodeHub <codehubx@gmail.com>
2021-10-15 19:26:39 +0200

    init

---
+++ 1_hillclimb.py
@@ -0,0 +1,103 @@
+#author: Candra Mulyadhi (1920996)
+#date: 11/10/2021
+
+## the path between cities is abbreviated as "p"
+## the total distance of a path is abbreviated as "d"
+
+import random
+import numpy as np
+import copy
+
+
+
+def generateRandomDistance():
+    return random.randrange(1, 1000)
+
+def initiateMap(amountCities):
+
+    # 2D Array of map
+    map = np.array([[0 for x in range(amountCities)] for y in range(amountCities)])
+
+    for i in range(0,amountCities):
+        for j in range(0,amountCities):
+            if i == j:
+                map[i][j] = 0
+            elif i < j:
+                distance = generateRandomDistance()
+                map[i][j] = distance
+                map[j][i] = distance
+
+    return map
+
+def createRandomPath(map):
+    path = np.array([i for i in range(0, len(map))])
+    return shuffle(path)
+
+
+
+def shuffle(path):
+    np.random.shuffle(path)
+    return path
+
+def calculateDistance(map, path):
+
+    distance = 0
+
+    for i in range(0,len(path)):
+        if i < len(path)-1:
+            distance += map[path[i]][path[i + 1]]
+
+    return distance
+
+def swapRandomNeighbors(path):
+
+    p = copy.copy(path)
+    idx = random.randrange(0, len(p)-1)
+
+    p[idx], p[idx+1] = p[idx+1], p[idx]
+
+    return p
+
+
+
+def findOptimalPath(map):
+
+    p = createRandomPath(map)
+    d = calculateDistance(map, p)
+
+    for i in range(0,500):
+
+        p2 = swapRandomNeighbors(p)
+        d2 = calculateDistance(map, p2)
+
+        if  d2 < d:
+            p = p2
+            d = d2
+
+    return p
+
+def findMinimum(map, reruns):
+
+    p = findOptimalPath(map)
+    d = calculateDistance(map, p)
+
+    print("\nDistance: ", d)
+    print("Path: ", p, "\n")
+
+    for i in range(0, reruns):
+        p2 = findOptimalPath(map)
+        d2 = calculateDistance(map, p2)
+        if d2 < d:
+            print("\nRerun: ", i + 1)
+            d = d2
+            p = p2
+
+            print("\n Distance: ", d, "\n", "Path: ", p)
+
+    print("\n\n\nFinal result: \n\n Distance: ", d, "\n Path: ", p)
+
+
+
+if __name__ == '__main__':
+    map = initiateMap(100)
+    findMinimum(map, 100)
---
+++ 3_genetic_algorithm.py
@@ -0,0 +1,162 @@
+# author: Candra Mulyadhi (1920996)
+# date: 11/10/2021
+
+## fitness:
+##
+
+
+import random
+import numpy as np
+import copy
+import math
+
+
+
+c = 0.001
+V = 100
+l = 100
+p = 10
+r = 0.8
+
+
+
+population = []
+newPopulation = []
+list = []
+
+
+
+def setList():
+
+    for x in range(l):
+        list.append(generateRandomVolume())
+
+    print(list)
+
+def generateRandomVolume():
+
+    return round(random.random()*10, 2)
+
+def createHypothesis():
+
+    hypothesis = []
+
+    for i in range(l):
+        bit = random.randrange(0,2)
+        hypothesis.append(bit)
+
+    return hypothesis
+
+def setPopulation():
+
+    for i in range(p):
+        population.append(createHypothesis())
+
+def selectHypothesis():
+
+    randNum = random.random()
+    sum = 0
+    idx = random.randrange(0, p)
+
+    while sum < randNum:
+        idx = idx + 1
+        idx = idx % p
+        sum = sum + pr(idx)
+
+    return idx
+
+def pr(i):
+
+    return fitness(i) / totalFitness()
+
+def getWeight(i):
+
+    w = 0
+
+    for j in range(l):
+        if population[i][j] == 1:
+            w += list[j]
+
+    return w
+
+def fitness(i):
+
+    w = getWeight(i)
+
+    return math.e ** -c*(V-getWeight(i))**2
+
+def totalFitness():
+
+    f = 0.0
+
+    for j in range(p):
+        f += fitness(j)
+
+    return f
+
+
+
+
+def selection():
+
+    for i in range(int((1-r)*p)):
+
+        selectedIndividual = population[selectHypothesis()]
+        newPopulation.append(selectedIndividual)
+
+def crossover():
+
+    for i in range(int(r*p)):
+
+        randIdx = selectHypothesis()
+        crossoverPoint = random.randrange(0,l)
+
+        dna1 = population[randIdx]
+        dna2 = population[(randIdx+1)%p]
+        copyDNA1 = copy(dna1)
+
+        for j in range(crossoverPoint):
+            dna1[j] = dna2[j]
+            dna2[j] = copyDNA1[j]
+
+def mutation():
+
+    for i in range(p):
+        if random.randrange(0,math.pow(c, -1)) == 1:
+            bitSwitch(population[i])
+
+def bitSwitch(i):
+
+    randIdx = random.randrange(0,1)
+
+    for j in i:
+        if randIdx == j:
+            if i[j] == 0:
+                i[j] = 1
+            else:
+                i[j] = 0
+
+
+
+def run(generations):
+
+    setList()
+    setPopulation()
+    f = totalFitness()
+
+    for generation in range(generations):
+
+        selection()
+        crossover()
+        mutation()
+
+        population = newPopulation
+        f = totalFitness()
+
+    return f
+
+
+
+if __name__ == '__main__':
+
+    run(50)
