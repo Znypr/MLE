1524c635ac57e957ead08e187ab73d72e4a133da
Znypr <mulyadhic@gmail.com>
2021-10-15 23:50:29 +0200

    bug: identical genes at higher generation

--- 3_genetic_algorithm.py
+++ 3_genetic_algorithm.py
@@ -1,59 +1,51 @@
 # author: Candra Mulyadhi (1920996)
 # date: 11/10/2021
 
-## fitness:
-##
+# fitness:
+#
 
 
 import random
-import numpy as np
 import copy
 import math
 
-
-
-c = 0.001
 V = 100
 l = 100
 p = 10
+
+c = 0.001
 r = 0.8
+m = 0.3
 
-
-
+list = []
 population = []
 newPopulation = []
-list = []
 
 
 
 def setList():
-
     for x in range(l):
         list.append(generateRandomVolume())
 
-    print(list)
-
-def generateRandomVolume():
-
-    return round(random.random()*10, 2)
+def setPopulation():
+    for i in range(p):
+        population.append(createHypothesis())
 
 def createHypothesis():
-
     hypothesis = []
 
     for i in range(l):
-        bit = random.randrange(0,2)
+        bit = random.randrange(0, 2)
         hypothesis.append(bit)
 
     return hypothesis
 
-def setPopulation():
+def generateRandomVolume():
+    return random.uniform(1,10)
 
-    for i in range(p):
-        population.append(createHypothesis())
+
 
 def selectHypothesis():
-
     randNum = random.random()
     sum = 0
     idx = random.randrange(0, p)
@@ -66,8 +58,29 @@ def selectHypothesis():
     return idx
 
 def pr(i):
-
     return fitness(i) / totalFitness()
+
+def fitness(i):
+    w = getWeight(i)
+
+    return math.e ** (-c * ((V - w) ** 2))
+
+def totalFitness():
+    f = 0
+
+    for j in range(p):
+        f += fitness(j)
+    return f
+
+def maxFitness():
+
+    max = 0
+
+    for i in range(p):
+        f = fitness(i)
+        if f > max:
+            max = f
+    return max
 
 def getWeight(i):
 
@@ -79,86 +92,114 @@ def getWeight(i):
 
     return w
 
-def fitness(i):
+def getWeights():
 
-    w = getWeight(i)
+    w = []
+    for i in range(p):
+        w.append(round(getWeight(i)))
 
-    return math.e ** -c*(V-getWeight(i))**2
-
-def totalFitness():
-
-    f = 0.0
-
-    for j in range(p):
-        f += fitness(j)
-
-    return f
-
+    return w
 
 
 
 def selection():
-
-    for i in range(int((1-r)*p)):
-
+    x = round((1-r)*p)
+    for i in range(x):
         selectedIndividual = population[selectHypothesis()]
         newPopulation.append(selectedIndividual)
 
 def crossover():
-
-    for i in range(int(r*p)):
+    x = round(r*p/2)
+    for i in range(x):
 
         randIdx = selectHypothesis()
-        crossoverPoint = random.randrange(0,l)
+        crossoverPoint = random.randrange(0, l)
 
-        dna1 = population[randIdx]
-        dna2 = population[(randIdx+1)%p]
-        copyDNA1 = copy(dna1)
+        h1 = population[randIdx]
+        h2 = population[(randIdx + 1) % p]
+        c1 = copy.copy(h1)
 
-        for j in range(crossoverPoint):
-            dna1[j] = dna2[j]
-            dna2[j] = copyDNA1[j]
+        for idx in range(crossoverPoint):
+            h1[idx] = h2[idx]
+            h2[idx] = c1[idx]
+
+        newPopulation.append(h1)
+        newPopulation.append(h2)
 
 def mutation():
-
-    for i in range(p):
-        if random.randrange(0,math.pow(c, -1)) == 1:
-            bitSwitch(population[i])
+    x = round(m*p)
+    for i in range(x):
+        randIdx = random.randrange(0, len(newPopulation))
+        bitSwitch(newPopulation[randIdx])
 
 def bitSwitch(i):
+    randIdx = random.randrange(0, l)
 
-    randIdx = random.randrange(0,1)
-
-    for j in i:
-        if randIdx == j:
-            if i[j] == 0:
-                i[j] = 1
-            else:
-                i[j] = 0
+    if i[randIdx] == 0:
+        i[randIdx] = 1
+    else:
+        i[randIdx] = 0
 
 def update():
+
+    global newPopulation
+    global population
+
     population = newPopulation
+    newPopulation = []
 
-
-def run(generations):
+def runWithGenerations(generations):
 
     setList()
     setPopulation()
-    f = totalFitness()
+    f = maxFitness()
+    start = f
+    g = 0
 
     for generation in range(generations):
-
+        g = g + 1
         selection()
         crossover()
         mutation()
+
+        update()
+        w = getWeights()
+
+        f = maxFitness()
+        print("Generation: ", g, "\nFitness: ", f, "\n")
+
+    print("Delta: ", f - start)
+
+def runWithTreshold(treshold):
+
+    setList()
+    setPopulation()
+    f = maxFitness()
+    start = f
+    g = 0
+
+    while f < treshold:
+        g = g + 1
+        selection()
+        crossover()
+        mutation()
+
         update()
 
-        #f = totalFitness()
+        f = maxFitness()
+        print("Generation: ", g, "\nFitness: ", f, "\n")
 
-    return f
+    print("Delta: ", f - start)
+
+def run(input):
+
+    if input < 1:
+        runWithTreshold(input)
+    else:
+        runWithGenerations(input)
 
 
 
 if __name__ == '__main__':
 
-    run(50)
+    run(100)
