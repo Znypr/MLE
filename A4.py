8205385402da011df400c1ea5fb753cff50e05ef
Znypr <mulyadhic@gmail.com>
2021-10-25 20:11:47 +0200

    functions

--- A4.py
+++ A4.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import time
 
@@ -53,56 +54,20 @@ class BasicGame(GameGL):
     windowName = "PingPong"
     pixelSize = 30
 
-    xMatrix = 4
-    yMatrix = 6
+    xMatrix, yMatrix = 4, 6
 
-    xBall = xMatrix//2
-    yBall = yMatrix//2
+    xBall, yBall = xMatrix//2, yMatrix//2
 
-    xPlayer = xMatrix//2
-    wPlayer = 2
+    xPlayer, wPlayer = xMatrix//2, 2
 
-    xV = 1
-    yV = 1
+    xV, yV = 1, 1
+
 
     score = 0
+    gamma = 0.9 #[0,1]
+    epsilon = 0.1
 
-
-    def simulateBallMovement(self, x, y, xV, yV):
-
-        x += xV
-        y += yV
-
-        if (x >= self.xMatrix-1 or x < 1):
-            xV = -xV
-        if (y >= self.yMatrix-1 or y < 1):
-            yV = -yV
-
-        return x,y, xV, yV
-
-    def calculateProjection(self):
-
-        x, y = self.xBall, self.yBall
-        xV, yV = self.xV, self.yV
-
-        while y > 1:
-            x, y, xV, yV = self.simulateBallMovement(x, y, xV, yV)
-
-        return x
-
-    def getRewards(self, projectedTarget):
-
-        rewards = np.ones((self.xMatrix - self.wPlayer + 1,), dtype=int) * -1
-
-        for i in range(self.wPlayer):
-            if projectedTarget < len(rewards) and projectedTarget >= 0:
-                rewards[projectedTarget] = 1
-
-            projectedTarget = projectedTarget - 1
-        return rewards
-
-
-
+    # ENGINE
     def __init__(self, name, width=pixelSize*xMatrix, height=pixelSize*yMatrix):
         super
         self.windowName = name
@@ -114,90 +79,6 @@ class BasicGame(GameGL):
         if key == b'\x1b':
             glutLeaveMainLoop()
             sys.exit(0)
-
-
-
-    def movePlayer(self):
-
-        action = 2.0 * np.random.random() - 1.0
-        if action < -0.3:
-            self.xPlayer -= 1
-        if action > 0.3:
-            self.xPlayer += 1
-
-    def limitPlayerReach(self):
-        # limit players reach
-        if self.xPlayer < 0:
-            self.xPlayer = 0
-        if self.xPlayer+self.wPlayer > self.xMatrix:
-            self.xPlayer = self.xMatrix-self.wPlayer
-
-
-    def moveBall(self):
-        self.xBall += self.xV
-        self.yBall += self.yV
-
-    def bounceBall(self):
-        if (self.xBall >= self.xMatrix-1 or self.xBall < 1):
-            self.xV = -self.xV
-        if (self.yBall >= self.yMatrix-1 or self.yBall < 1):
-            self.yV = -self.yV
-
-    def handleCollision(self, rewards):
-        # check whether ball on bottom line
-        if self.yBall == 1 and self.yV == -1:
-            self.score += rewards[self.xPlayer]
-            # player-ball collision
-            if self.xBall >= self.xPlayer and self.xBall <= self.xPlayer+self.wPlayer-1:
-                # bounce ball on player
-                self.yV = -self.yV
-                print("HIT > ", self.score)
-            else:
-                print("MISS > ", self.score)
-
-            print(rewards)
-
-
-    def getState(self):
-        projection = self.calculateProjection()
-        rewards = self.getRewards(projection)
-
-        return rewards
-
-    def run(self):
-
-        self.display()
-
-        s = self.getState()
-
-        self.movePlayer()
-        self.limitPlayerReach()
-
-        self.moveBall()
-        self.bounceBall()
-
-        self.handleCollision(s)
-
-
-        self.drawBall()
-        self.drawPlayer()
-
-        # adaptive speed depending on matrix size
-        time.sleep(2/((self.xMatrix+self.yMatrix)))
-
-        glutSwapBuffers()
-
-    def display(self):
-        # clear the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # reset position
-        glLoadIdentity()
-        glViewport(0, 0, self.width, self.height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0.0, self.width, 0.0, self.height, 0.0, 1.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
 
     def start(self):
         glutInit()
@@ -219,6 +100,42 @@ class BasicGame(GameGL):
     def onResize(self, width, height):
         self.width = width
         self.height = height
+
+    def run(self):
+
+        self.display()
+
+        s = self.getState()
+
+        self.movePlayer()
+        self.limitPlayerReach()
+
+        self.moveBall()
+        self.bounceBall()
+
+        self.handleCollision(s)
+
+        self.drawBall()
+        self.drawPlayer()
+
+        # adaptive speed depending on matrix size
+        time.sleep(2 / ((self.xMatrix + self.yMatrix)))
+
+        glutSwapBuffers()
+
+
+    # VISUALISATION
+    def display(self):
+        # clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # reset position
+        glLoadIdentity()
+        glViewport(0, 0, self.width, self.height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, self.width, 0.0, self.height, 0.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
     def drawBall(self, width=1, height=1, x=xBall, y=yBall, color=(0.0, 1.0, 0.0)):
         x = self.xBall
@@ -255,6 +172,125 @@ class BasicGame(GameGL):
         # top left point
         glVertex2f(xPos, (self.pixelSize * height))
         glEnd()
+
+
+    # PHYSICS
+    def movePlayer(self):
+
+        action = 2.0 * np.random.random() - 1.0
+        if action < -0.3:
+            self.xPlayer -= 1
+        if action > 0.3:
+            self.xPlayer += 1
+
+    def limitPlayerReach(self):
+        # limit players reach
+        if self.xPlayer < 0:
+            self.xPlayer = 0
+        if self.xPlayer+self.wPlayer > self.xMatrix:
+            self.xPlayer = self.xMatrix-self.wPlayer
+
+    def moveBall(self):
+        self.xBall += self.xV
+        self.yBall += self.yV
+
+    def bounceBall(self):
+        if (self.xBall >= self.xMatrix-1 or self.xBall < 1):
+            self.xV = -self.xV
+        if (self.yBall >= self.yMatrix-1 or self.yBall < 1):
+            self.yV = -self.yV
+
+    def handleCollision(self, rewards):
+        # check whether ball on bottom line
+        if self.yBall == 1 and self.yV == -1:
+            self.score += rewards[self.xPlayer]
+            # player-ball collision
+            if self.xBall >= self.xPlayer and self.xBall <= self.xPlayer+self.wPlayer-1:
+                # bounce ball on player
+                self.yV = -self.yV
+                print("HIT > ", self.score)
+            else:
+                print("MISS > ", self.score)
+
+            print(rewards)
+
+
+    # LEARNING
+    '''
+        def simulateBallMovement(self, x, y, xV, yV):
+    
+            x += xV
+            y += yV
+    
+            if (x >= self.xMatrix-1 or x < 1):
+                xV = -xV
+            if (y >= self.yMatrix-1 or y < 1):
+                yV = -yV
+    
+            return x,y, xV, yV
+    
+        def calculateProjection(self):
+    
+            x, y = self.xBall, self.yBall
+            xV, yV = self.xV, self.yV
+    
+            while y > 1:
+                x, y, xV, yV = self.simulateBallMovement(x, y, xV, yV)
+    
+            return x
+    
+        def getRewards(self, projectedTarget):
+    
+            rewards = np.ones((self.xMatrix - self.wPlayer + 1,), dtype=int) * -1
+    
+            for i in range(self.wPlayer):
+                if projectedTarget < len(rewards) and projectedTarget >= 0:
+                    rewards[projectedTarget] = 1
+    
+                projectedTarget = projectedTarget - 1
+            return rewards
+    
+        def getState(self):
+            projection = self.calculateProjection()
+            rewards = self.getRewards(projection)
+    
+            return rewards
+    '''
+
+    def getState(env, limit):
+        s = env[0]
+        for i in range(1,len(env)):
+            s = s*limit[i] + env[i]
+        return s
+
+    def selectAction(self, a, greedy):
+        if greedy:
+            return a[random.randrange(0,a.length)]
+        else:
+            return max(a)
+
+    def select_greedy(self, a):
+        p = random.random()
+        if p>self.epsilon:
+            return selectAction(a, false)
+        else:
+            return selectAction(a, true)
+
+    def max(arr, max=0):
+        for i in range(arr.length):
+            if arr[i]>max:
+                max=arr[i]
+        return max
+
+    def Q(self, t, state, action):
+        # return discounted reward for (s,a)
+        reward = 0
+        while true:
+            reward += self.gamma**i * reward[t+i]
+
+
+
+
 
 
 if __name__ == '__main__':
